9c101876b90ce8fc5639a04f7bbe89d9c9f065db
Pratyush061 <142402529+Pratyush061@users.noreply.github.com>
2025-01-25 23:16:02 +0530

    Update serviceWorkerRegistration.js
    modify the file for vercel compatibility

--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -8,16 +8,10 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-
-    // Ensure service worker is used only on the same origin
-    if (publicUrl.origin !== window.location.origin) {
-      return;
-    }
+    // Service Worker URL fixed for Vercel deployment
+    const swUrl = `/service-worker.js`;
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         // Check the service worker on localhost for development purposes
         checkValidServiceWorker(swUrl, config);
